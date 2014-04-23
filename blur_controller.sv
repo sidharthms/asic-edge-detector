8edf313428e20aa900676679632cdb00f7f6a18e
Sidharth Mudgal <sidharthmsk@gmail.com>
2014-04-23 01:28:35 -0400

    Blur controller organized, cyclic add concept added.

--- blur_controller.sv
+++ blur_controller.sv
@@ -17,12 +17,13 @@ module blur_controller
 
   input  wire [7:0] blur_in [20];
   output wire [7:0] blur_out [16];
-  output reg filter_final            // Filter phase completed for all pixels.
+  output reg blur_final;             // Filter phase completed for all pixels.
 );
   
-  reg [7:0] blur_data [5][16];
-  reg [7:0] blur_data_new [20];
-  reg [3:0] first_column;
+  reg  [7:0] blur_data [5][16];
+  reg  [7:0] blur_data_new [20];
+  reg  [2:0] first_column;
+  wire [2:0] stale_column;
 
   typedef enum {IDLE, COPY, PROCESS, FINAL} state_type;
   state_type state, next_state;
@@ -32,61 +33,69 @@ module blur_controller
   wire on_last;
   reg stage;
 
+  wire unit_en;
   wire [7:0] in_pixels [5];
   wire [7:0] out_pixel;
-  reg  direction;
+  wire unit_final;
 
-  assign clear = state == IDLE;
+  assign clear = next_state == IDLE;
+  assign unit_en = next_state == PROCESSING;
 
   flex_counter #(4) index_counter(
       .clk(clk),
       .n_rst(n_rst),
       .clear(clear),
-      .count_enable(1),
-      .rollover_val(4'd15),
+      .count_enable(unit_final),
+      .rollover_val(15),
       .count_out(index),
       .rollover_flag(on_last));
 
   blur_filter filter(
+      .en(unit_en),
       .in_pixels(in_pixels),
       .out_pixel(out_pixel),
-      .direction(direction));
+      .final(unit_final));
 
-  column_shift #(.BITS(8), WIDTH(5)) data_shift(
+  column_shift #(
+      .BITS(8),
+      .WIDTH(5),
+      .SHIFT_BITS(3))
+    data_shift(
       .columns(blur_data[index]),
+      .shift(first_column),
       .shifted_columns(shifted_blur_data));
 
-  assign filter_final = stage == 1 && on_last;
+  cyclic_add #(
+      .BITS(3))
+    subtract_one(
+      .left(first_column),
+      .right(-1),
+      .result(stale_column));
+
+  assign blur_final = stage == 1 && on_last;
 
   always @ (posedge clk, negedge n_rst)
   begin
     if (n_rst == 1'b0)
-    begin
       state <= IDLE;
-      first_column <= 0;
-    end
     else
       state <= next_state;
 
-    if (next_state == IDLE)
+    if (next_state == COPY)
       stage <= 0;
     else if (on_last)
       stage <= 1;
 
     if (next_state == COPY)
       blur_data_new <= blur_in;
-    
-    if (next_state ~= IDLE)
-    begin
-      if (stage == 0)
-        
-        in_pixel = blur_data_new[index +: 5];
-      else
-        in_pixel = shifted_blur_data;
-    end
+
+    if (stage == 0)
+      blur_data[row][stale_column] <= out_pixel;
+    else
+      blur_out[row] <= out_pixel;
   end
 
-  always @ (state, anchor_moving, filter_final)
+  always @ (state, anchor_moving, blur_final)
   begin
     case (state)
       IDLE:
@@ -100,7 +109,7 @@ module blur_controller
         next_state = PROCESSING;
       PROCESSING:
       begin
-        if (filter_final)
+        if (blur_final)
         begin
           if (anchor_moving)
             next_state = COPY;
@@ -113,7 +122,7 @@ module blur_controller
     end
   end
 
-  always @ (next_state,
+  always @ (next_state, stage, index)
   begin
     if (stage == 0)
       in_pixels = blur_data_new[index +: 5];
