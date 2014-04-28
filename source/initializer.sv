194037d565eb8df4dc8e721870569fc85c9dde31
ssabpisa <ssabpisa@me.com>
2014-04-28 12:02:53 -0400

    Some more initializer logic (WIP)

--- source/initializer.sv
+++ source/initializer.sv
@@ -1,21 +1,23 @@
 // $Id: $
-// File name:   bus_iface
+// File name:   initializer
 // Created:     4/23/2014
 // Author:      Suppatach Sabpisal
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
 // $Id: $
-// File name:   bus_iface
+// File name:   initializer
 // Created:     4/23/2014
 // Author:      Suppatach Sabpisal
 // Lab Section: 337-03
 // Version:     1.0  Initial Design Entry
-// Description: Bus Interface - read data from AHB BUS
+// Description: Listens for request on AHB BUS
+//		and starts controllers when start sequence is found
 
 parameter BUSWIDTH = 32;
 
 module initializer
 (
+	input wire n_rst,
     	input wire ahb_hclk, //bus clock
    	input wire [1:0] ahb_htrans, //transfer kind
 	input wire [2:0] ahb_hburst, //burst kind
@@ -31,7 +33,7 @@ module initializer
 	output wire ahb_hresp //transfer response
 );
 
-   typedef enum     {IDLE, READ_DIM, READ_ADDR1, READ_ADD2, READ_FILTER, KICKSTART} state_type;
+   typedef enum {IDLE, READ_DIM, READ_ADDR1, READ_ADD2, READ_FILTER, KICKSTART} state_type;
    state_type state, next_state;
    
    reg width;
@@ -39,16 +41,28 @@ module initializer
    reg readStartAddress;
    reg writeStartAddress;
    reg filterType;
-   
-    /*flex_counter #(.NUM_CNT_BITS(4)) index_counter(
-      .clk(clk),
+
+   reg flex_clear, flex_en;   
+   reg [3:0] flex_out;
+   reg flex_done;
+
+    flex_counter #(.NUM_CNT_BITS(4)) cycle_counter(
+      .clk(ahb_clk),
       .n_rst(n_rst),
-      .clear(clear),
-      .count_enable(unit_final),
+      .clear(flex_clear),
+      .count_enable(flex_en),
       .rollover_val(15),
-      .count_out(index),
-      .rollover_flag(on_last));*/
-   always @
+      .count_out(flex_out),
+      .rollover_flag(flex_done));
+  
+   always @ (posedge ahb_hclk, negedge n_rst)
+   begin
+      if(n_rst == 1'b1) begin
+	 state = next_state;
+      end else if(n_rst == 1'b0) begin
+         state = IDLE;
+         //TODO: reset all other signals
+      end
+   end
  
-   end 
 endmodule
