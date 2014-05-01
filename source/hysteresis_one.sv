f4ad323f3781de89af058e40be4643544f11e4ba
beaslrya <beaslrya@gmail.com>
2014-05-01 02:57:44 -0400

    Hysteresis worked on needs TB

--- source/hysteresis_one.sv
+++ source/hysteresis_one.sv
@@ -16,24 +16,23 @@ module hysteresis_one
 	output reg [7:0] out_pixel
 );
 
-
-always_comb begin
+always @ (*) begin
 //ANGLE 1
   if(grad_in_angle==0)begin
     if(grad_in_mag[3]==8'b11111111)begin
       if(grad_in_mag[4]>=lowthresh)begin
-	grad_in_mag[4] = 8'b11111111;
+	out_pixel = 255;
       end //lowthresh if
       else begin
-	grad_in_mag[4] = 8'b00000000;
+	out_pixel = 8'b00000000;
       end //else lowthresh
     end   //compare if mag
     else begin
       if(grad_in_mag[4]>=upthresh)begin
-	grad_in_mag[4]=8'b11111111;
+	out_pixel = 8'b11111111;
       end //upthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else upthresh
     end   //else compare mag
   end
@@ -41,18 +40,18 @@ always_comb begin
   else if(grad_in_angle==1)begin
     if(grad_in_mag[2]==8'b11111111)begin
       if(grad_in_mag[4]>=lowthresh)begin
-        grad_in_mag[4]=8'b11111111;
+        out_pixel = 8'b11111111;
       end //lowthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else lowthresh
     end   //compare if mag
     else begin
       if(grad_in_mag[4]>=upthresh)begin
-        grad_in_mag[4]=8'b11111111;
+        out_pixel = 8'b11111111;
       end //upthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else upthresh
     end   //else compare mag
   end
@@ -60,18 +59,18 @@ always_comb begin
   else if(grad_in_angle==2)begin
     if(grad_in_mag[1]==8'b11111111)begin
       if(grad_in_mag[4]>=lowthresh)begin
-        grad_in_mag[4]=8'b11111111;
+        out_pixel = 8'b11111111;
       end //lowthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else lowthresh
     end   //compare if mag
     else begin
       if(grad_in_mag[4]>=upthresh)begin
-        grad_in_mag[4]=8'b11111111;
+        out_pixel = 8'b11111111;
       end //upthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else upthresh
     end   //else compare mag
   end
@@ -79,18 +78,18 @@ always_comb begin
   else if(grad_in_angle==3)begin
     if(grad_in_mag[0]==8'b11111111)begin
       if(grad_in_mag[4]>=lowthresh)begin
-        grad_in_mag[4]=8'b11111111;
+        out_pixel = 8'b11111111;
       end //lowthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else lowthresh
     end   //compare if mag
     else begin
       if(grad_in_mag[4]>=upthresh)begin
-        grad_in_mag[4]=8'b11111111;
+        out_pixel = 8'b11111111;
       end //upthresh if
       else begin
-        grad_in_mag[4]=8'b00000000;
+        out_pixel = 8'b00000000;
       end //else upthresh
     end   //else compare mag
   end//always block
