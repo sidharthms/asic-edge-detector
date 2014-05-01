b24547ca25a74d97f6d136208222d8e5b60e3def
hxiong <hxiong@purdue.edu>
2014-04-30 21:05:58 -0400

    works for converting from bit string to gray scale image

--- source/image_str_converter.py
+++ source/image_str_converter.py
@@ -63,7 +63,6 @@ def convertToString(im_filename,output_filename):
 
 
 
-
 def convertToImage(filename_input,filename_output):
     fptr=open(filename_input,'r')
     lines=fptr.readlines()
@@ -139,4 +138,85 @@ def convertToImage(filename_input,filename_output):
 
     im.save(filename_output)
             
+   
+
+def convertToGrayImage(filename_input,filename_output):
+    fptr=open(filename_input,'r')
+    lines=fptr.readlines()
+
+    del lines[0]
+
+    #extract output image info 
+    line1_str=lines[0].replace(":"," ")
+    line1_str=line1_str.replace(";"," ")
+
+    line2_str=lines[1].replace(":"," ")
+    line2_str=line2_str.replace(";"," ")
     
+    line1_lst=line1_str.split()
+    line2_lst=line2_str.split()
+
+    wid_hex=line1_lst[1][4:]
+    hei_hex=line2_lst[1][4:]
+
+    bv_temp1=BitVector(hexstring=wid_hex)
+    bv_temp2=BitVector(hexstring=hei_hex)
+
+    image_size=(int(bv_temp1),int(bv_temp2))
+
+    #delete list except for the image data
+    del lines[0:2]
+    
+    # create a list for image bytes in hex form 
+    list_hex=[]
+    for i in range(len(lines)):
+        line_str=lines[i].replace(":"," ")
+
+        line_str=line_str.replace(";"," ")
+        line_lst=line_str.split()
+
+        # get the hex value as a string of each pixel bytes
+        hex_str=line_lst[1]
+        list_hex.append(hex_str[4:])
+   
+
+    print "length of first pixel is:",len(list_hex[0])
+    if len(list_hex[0]) == 2:
+        pass
+    else:
+        raise ValueError("wrong bit for the pixel,not equals to 2")
+
+    # convert to hex string of image
+    string_hex="".join(list_hex)
+
+    # bv of bits of an image
+    bv=BitVector(hexstring=string_hex)
+
+    # list contains byte value of pixels of an image
+    pxl_byte_list=[]
+
+    # convert bits to bytes
+    index=0
+    while index<len(bv):
+        pxl_byte_list.append(int(bv[index:index+8]))
+        index=index+8
+   
+    # create a new image object with image_mode str and image_size tuple passed
+    image_mode='L'
+    im=Image.new(image_mode,image_size)
+    im=im.convert(image_mode)
+    pixels_map=im.load()
+
+    print "image size is:",image_size
+
+    # pixel mapping all the pixels with bytes from the bytelist
+    ind=0
+    print "length of pixel byte list:",len(pxl_byte_list)
+    for h in range(image_size[0]):
+        for w in range(image_size[1]):
+            pixels_map[w,h]=(pxl_byte_list[ind])
+            ind=ind+1
+
+    print im
+    im.save(filename_output)
+ 
--- source/run_im_bit.py
+++ source/run_im_bit.py
@@ -5,5 +5,6 @@ from image_str_converter import *
 
 if __name__=="__main__":
     #convertToString('lena.bmp','output.mem')
-    convertToImage('output.mem','lena_recovered.bmp')
+    #convertToImage('output.mem','lena_recovered.bmp')
+    convertToGrayImage('aoutputs.mem','lena_grayscale.bmp')
 
